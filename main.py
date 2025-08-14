c3d539f3ccea68883674ef42dab08bcfc826b94b
Ilonczai András <ilonczai.andras16@gmail.com>
2025-08-14 18:44:28 +0200

    Gitignore extended with .json files to ignore them

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from scrapers.site_b_scraper.site_b_scraper import process_all_legislatura_data
 from config.settings import scrape_targets, outputTargets
 
 def createJsonFiles():
+
     logging.info("Creating legislatura json's")
-    
     scrape_all_legislaturas(urls_to_scrape=scrape_targets)
 
 def run_scrapers():
+
     logging.info("Extending the legislatura.json")
     process_all_legislatura_data(urls_to_scrape=outputTargets)
+    
 if __name__ == "__main__":
     logging.info("Starting web scraping process...")
 
-    #createJsonFiles()
+    createJsonFiles()
     run_scrapers()
